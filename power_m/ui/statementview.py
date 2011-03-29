557257235cea689472d093debd94b4209b1e262f
YLM <ylm@ylm-laptop.(none)>
2011-03-29 17:39:27 +0000

    Amélioration du statementview

--- power_m/ui/statementview.py
+++ power_m/ui/statementview.py
@@ -31,11 +31,18 @@ class AddstatementViewWidget(QtGui.QDialog, PowerWidget):
         titelebox.addWidget(QtGui.QLabel((u'Type')))
         titelebox.addWidget(QtGui.QLabel((u'Value')))
 
+        liste_type = ["Solde", "Ajout", "Coupure", "Reprise"]
+        #Combobox widget
+        self.box_type = QtGui.QComboBox()
+        for index in liste_type:
+            self.box_type.addItem((u'%(type)s') % {'type': index})
+        
         editbox = QtGui.QHBoxLayout()
         editbox.addWidget(self.date_)
-        editbox.addWidget(self.type_)
+        editbox.addWidget(self.box_type)
         editbox.addWidget(self.value_)
 
+                        
         button_hbox = QtGui.QHBoxLayout()
         butt = QtGui.QPushButton((u"Add"))
         butt.clicked.connect(self.add_statement)
@@ -58,12 +65,12 @@ class AddstatementViewWidget(QtGui.QDialog, PowerWidget):
 
     def add_statement(self):
         ''' add statement '''
-
         year, month, day = self.date_.text().split('-')
         date_ = datetime(int(year), int(month), int(day))
 
         if self.date_ and self.type_ and self.value_:
-            operation = Operation(date_, unicode(self.type_.text()),\
+            dic = {0:"S",1:"A",2:"C",3:"R"}
+            operation = Operation(date_, unicode(dic[self.box_type.currentIndex()]),\
                             unicode(self.value_.text()))
             session.add(operation)
             session.commit()
