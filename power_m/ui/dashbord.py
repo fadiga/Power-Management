95e3c8abffcc277a8af2b7a39f549c8977e963af
Alou Dolo <aloud7@gmail.com>
2011-03-30 15:49:03 +0000

    Did you mean: QVBoxLayout.addLayout() to add a box containing another QLayout to the row and set that layout’s stretch factor.

--- power_m/ui/dashbord.py
+++ power_m/ui/dashbord.py
@@ -13,15 +13,35 @@ class DashbordViewWidget(PowerWidget):
         super(DashbordViewWidget, self).__init__(parent=parent,
                                                         *args, **kwargs)
         self.title = PowerPageTitle(u"Dashbord")
-        self.table = DashbordTableWidget(parent=self)
+        self.table1 = SoldeTableWidget(parent=self)
+        self.table2 = ConsomationTableWidget(parent=self)
         vbox = QtGui.QVBoxLayout()
-
         vbox.addWidget(self.title)
-        vbox.addWidget(self.table)
+        tablebox1 = QtGui.QHBoxLayout()
+        tablebox2 = QtGui.QHBoxLayout()
+        tablebox1.addWidget(self.table1)
+        tablebox2.addWidget(self.table2)
+        vbox.addLayout(tablebox1)
+        vbox.addLayout(tablebox2)
         self.setLayout(vbox)
 
 
-class DashbordTableWidget(PowerTableWidget):
+class SoldeTableWidget(PowerTableWidget):
+
+    def __init__(self, parent, *args, **kwargs):
+
+        PowerTableWidget.__init__(self, parent=parent, *args, **kwargs)
+        self.header = [_(u"Date"), _(u"Type"), \
+                        _(u"Valeur")]
+        self.set_data_for()
+        self.refresh(True)
+
+    def set_data_for(self):
+        self.data = [(op.date_op.strftime(u'%d-%m-%Y'), op.type, op.valeur)
+            for op in session.query(Operation).all()]
+
+
+class ConsomationTableWidget(PowerTableWidget):
 
     def __init__(self, parent, *args, **kwargs):
 
