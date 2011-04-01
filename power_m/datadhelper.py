74e023dd1d7053ceeaf68649735703046849d050
Alou Dolo <aloud7@gmail.com>
2011-04-01 17:33:55 +0000

    dashbord.py

---
+++ power_m/datadhelper.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# maintainer: alou
+
+from database import *
+from datetime import date, datetime
+from sqlalchemy import desc, func
+
+
+def consomation(operations):
+    ''' total amount expendited during that period '''
+    operation_balances = session.query(Operation.valeur)
+               .filter(Operation.type=='Solde').all()
+
--- power_m/ui/dashbord.py
+++ power_m/ui/dashbord.py
@@ -3,6 +3,7 @@
 # maintainer: alou
 
 from PyQt4 import QtGui, QtCore
+from sqlalchemy import desc
 from database import *
 from common import PowerWidget, PowerPageTitle, PowerTableWidget
 from gettext import gettext as _
@@ -13,35 +14,42 @@ class DashbordViewWidget(PowerWidget):
         super(DashbordViewWidget, self).__init__(parent=parent,
                                                         *args, **kwargs)
         self.title = PowerPageTitle(u"Dashbord")
-        self.table1 = SoldeTableWidget(parent=self)
-        self.table2 = ConsomationTableWidget(parent=self)
+        self.title_alert = PowerPageTitle(u"Alert")
+        self.table_balance = BalanceTableWidget(parent=self)
+        self.table_consumption = ConsumptionTableWidget(parent=self)
         vbox = QtGui.QVBoxLayout()
+        hbox = QtGui.QHBoxLayout()
+        hbox_alert = QtGui.QHBoxLayout()
+        hbox_alert.addWidget(self.title_alert)
         vbox.addWidget(self.title)
-        tablebox1 = QtGui.QHBoxLayout()
-        tablebox2 = QtGui.QHBoxLayout()
-        tablebox1.addWidget(self.table1)
-        tablebox2.addWidget(self.table2)
-        vbox.addLayout(tablebox1)
-        vbox.addLayout(tablebox2)
+        tablebox_balance = QtGui.QHBoxLayout()
+        tablebox_consumption = QtGui.QHBoxLayout()
+        tablebox_balance.addWidget(self.table_balance)
+        tablebox_consumption.addWidget(self.table_consumption)
+        hbox.addLayout(tablebox_balance)
+        hbox.addLayout(tablebox_consumption)
+        vbox.addLayout(hbox)
+        vbox.addLayout(hbox_alert)
         self.setLayout(vbox)
 
 
-class SoldeTableWidget(PowerTableWidget):
+class BalanceTableWidget(PowerTableWidget):
 
     def __init__(self, parent, *args, **kwargs):
 
         PowerTableWidget.__init__(self, parent=parent, *args, **kwargs)
         self.header = [_(u"Date"), _(u"Type"), \
-                        _(u"Valeur")]
+                        _(u"Value")]
         self.set_data_for()
         self.refresh(True)
 
     def set_data_for(self):
-        self.data = [(op.date_op.strftime(u'%d-%m-%Y'), op.type, op.valeur)
-            for op in session.query(Operation).all()]
+        self.data = [(op.date_op.strftime(u'%d-%m-%Y %Hh:%Mmn'), op.type, op.valeur)
+            for op in session.query(Operation).filter(Operation.type=='Solde').\
+                      order_by(desc(Operation.date_op)).all()]
 
 
-class ConsomationTableWidget(PowerTableWidget):
+class ConsumptionTableWidget(PowerTableWidget):
 
     def __init__(self, parent, *args, **kwargs):
 
